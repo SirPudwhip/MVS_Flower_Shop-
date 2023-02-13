ffc2484caad0fb8cf1e414389a24ae0cc0f994d3
SirPudwhip <spamfolder3399@gmail.com>
2023-02-13 12:05:01 -0500

    Added list of flowers to populate as well as some CSS that doesn't work

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-I'm testing to see if this goes away 
+fetch ('http://localhost:3000/flowers')
+.then((res) => (res.json()))
+.then((data) => data.forEach(populateList))
+
+let flowerCard = document.querySelector('.flower-list');
+
+function populateList(data) {
+    console.log(data.image);
+    let li = document.createElement('li');
+    let pName = document.createElement('p');
+    let pPrice = document.createElement('p');
+    let img = document.createElement('img');
+    let button = document.createElement('button');
+    let pInventory = document.createElement('p');
+    let ulComments = document.createElement('ul');
+
+    pName.textContent = data.name;
+    pPrice = data.price;
+    img.src = data.image
+    button.textContent = "Purchase"
+    pInventory = data.pInventory
+    li.className = "card"
+
+    flowerCard.append(li)
+    li.append(pName);
+    li.append(pPrice);
+    li.append(img);
+    li.append(button);
+    li.append(ulComments);
+
+} 
+
